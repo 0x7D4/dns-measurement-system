53062fd7ad6e74b29f7270737902587e7215998a
Aditya Ghosh <ghoshswati70@gmail.com>
2025-12-03 01:11:16 +0530

    updated run.sh

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ TRACEROUTE_TEST_DOMAIN = "ietf.org"     # used by traceroute test
 CACHE_TTL_TEST_DOMAIN = "isc.org"       # used by DNS cache TTL/invalidation test
 
 # Input and scheduling
-INPUT_FILE = "test.json"
+INPUT_FILE = os.getenv("INPUT_FILE", "test.json")
 LOOP_INTERVAL = 3600  # 1 hour
 DEFAULT_DELAY = 0.1
 DEFAULT_BATCH_SIZE = 100
